99cd57273b616971802638edaa56b672cd1d3667
Ellis <ellis@s-team.com.tw>
2016-06-27 16:24:28 +0800

    4.3.2. Introducing asynchronous behavior

--- src/test/java/org/bpmnwithactiviti/chapter04/JavaBpmnTest.java
+++ src/test/java/org/bpmnwithactiviti/chapter04/JavaBpmnTest.java
@@ -1,12 +1,17 @@
 package org.bpmnwithactiviti.chapter04;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import org.activiti.engine.RuntimeService;
+import org.activiti.engine.impl.ProcessEngineImpl;
+import org.activiti.engine.impl.jobexecutor.JobExecutor;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.test.ActivitiRule;
 import org.activiti.engine.test.Deployment;
@@ -25,6 +30,46 @@ public class JavaBpmnTest {
 		return runtimeService.startProcessInstanceByKey("bookorder", variableMap);
 	}
 	
+	private boolean waitForJobExecutorToProcessAllJobs(long maxMillisToWait, long intervalMillis){
+		Timer timer = new Timer();
+		InteruptTask task = new InteruptTask(Thread.currentThread());
+		timer.schedule(task, maxMillisToWait);
+		boolean areJobsAvailable = true;
+		try {
+			while (areJobsAvailable && !task.isTimeLimitExceeded()){
+				Thread.sleep(intervalMillis);
+				areJobsAvailable = areJobsAvailable();
+			}
+		} catch(InterruptedException e){
+		} finally{
+			timer.cancel();
+		}
+		return areJobsAvailable;
+	}
+	
+	public boolean areJobsAvailable(){
+		return !activitiRule.getManagementService()
+				.createJobQuery()
+				.executable()
+				.list()
+				.isEmpty();
+	}
+	
+	private static class InteruptTask extends TimerTask {
+		protected boolean timeLimitExceeded = false;
+		protected Thread thread;
+		public InteruptTask(Thread thread){
+			this.thread = thread;
+		}
+		public boolean isTimeLimitExceeded(){
+			return timeLimitExceeded;
+		}
+		public void run(){
+			timeLimitExceeded = true;
+			thread.interrupt();
+		}
+	}
+	
 	@Test
 	@Deployment(resources = {"chapter04/bookorder.java.bpmn20.xml"})
 	public void executeJavaService(){
@@ -34,4 +79,20 @@ public class JavaBpmnTest {
 		assertNotNull(validateTime);
 		System.out.println("validatetime is " + validateTime);
 	}
+	
+	@Test
+	@Deployment(resources = {"chapter04/bookorder.async.bpmn20.xml"})
+	public void executeAsyncService(){
+		JobExecutor jobExecutor = ((ProcessEngineImpl) activitiRule.getProcessEngine()).getProcessEngineConfiguration().getJobExecutor();
+		jobExecutor.start();
+		ProcessInstance processInstance = startProcessInstance();
+		System.out.println("Started process instance");
+		assertEquals(false, waitForJobExecutorToProcessAllJobs(5000, 100));
+		RuntimeService runtimeService = activitiRule.getRuntimeService();
+		Date validatetime = (Date) runtimeService.getVariable(processInstance.getId(), "validatetime");
+		assertNotNull(validatetime);
+		System.out.println("validatetime is " + validatetime);
+		jobExecutor.shutdown();
+	}
+	
 }
